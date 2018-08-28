19750ddcabe8761f84b71c174d0c9f68c58d9d60
mike schneider <mikehschneider@gmail.com>
2018-08-28 13:31:04 -0700

    live code examples

---
+++ Unit3and4/src/Unit3and4.java
@@ -0,0 +1,43 @@
+public class Unit3and4 {
+    public static void main(String[] args){
+
+        // hello tacoma on one line using print
+        System.out.print("Hello ");
+        System.out.print("Tacoma");
+
+        System.out.println();
+        System.out.println();
+
+        // hello tacoma on two lines using println
+        System.out.println("Hello ");
+        System.out.println("Tacoma");
+
+        System.out.println();
+        System.out.println();
+
+        /* hello tacoma on two lines using
+         the line feed escape sequence */
+        System.out.println("Hello \nTacoma");
+
+        System.out.println();
+        System.out.println();
+
+        /* hello tacoma in quotes using
+         the quote escape sequence */
+        System.out.println("\"Hello Tacoma\"");
+
+        System.out.println();
+        System.out.println();
+
+        /* hello tacoma with tab space
+        using the tab escape sequence */
+        System.out.println("Hello \tTacoma");
+
+        System.out.println();
+        System.out.println();
+
+       /* hello tacoma inside back slashes
+       the back slash escape sequence */
+       System.out.println("\\Hello Tacoma\\");
+    }
+}
