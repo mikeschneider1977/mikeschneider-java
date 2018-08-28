73292105dcdf2df1abde4fe69d0dedac625d034c
mike schneider <mikehschneider@gmail.com>
2018-08-28 13:29:09 -0700

    Pikachu sample code

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,11 @@
+public class Pikachu {
+    public static void main(String[] args){
+
+        System.out.println(" (\\__/)");
+
+        System.out.println(" (o^.^)");
+
+        System.out.println("z(_(\")(\")");
+    }
+}
+
